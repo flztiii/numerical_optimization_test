ed29f1514bde3e908b420aaa309779aa0891c581
Unknown <415569252@qq.com>
2020-06-10 22:34:19 +0800

    完成可视化

--- gradient_descent_vis.py
+++ gradient_descent_vis.py
@@ -32,14 +32,15 @@ def main():
     print('true min value is:', f(ground_truth))
 
     # visualization prepare
-    fig = plt.figure()
+    fig = plt.figure(figsize=(14,14))
     ax = fig.add_subplot(111, projection='3d')
     
     # set initial point
-    x = np.array([0.0, 0.0]).T
+    x = np.array([-1.0, -1.0]).T
     learning_rate = 0.01
-    threshold = 1e-10
+    threshold = 1e-4
     iteration_num = 0
+    recorder = [x]
     
     gradient = df(x)
     # start iteration
@@ -50,22 +51,53 @@ def main():
         gradient = df(x)
         # update num
         iteration_num += 1
+        # update recorder
+        recorder.append(x)
 
-        # visualization surface
-        if iteration_num%10 == 0:
-            X = np.arange(-2, 2, 0.1)
-            Y = np.arange(-2, 2, 0.1)
+       # visualization
+        if iteration_num%2 == 0:
+            plt.cla()
+            # visualize surface
+            X = np.arange(-1.5, 1.5, 0.1)
+            Y = np.arange(-1.5, 1.5, 0.1)
             X, Y = np.meshgrid(X,Y)
             Z = np.zeros_like(X)
             for i in range(0, X.shape[0]):
                 for j in range(0, X.shape[1]):
                     Z[i, j] = f(np.array([X[i, j], Y[i, j]]).T)
             ax.plot_surface(X, Y, Z)
-            plt.pause(0.01)
-
+            # visualize gradient descent
+            recorder_x, recorder_y, recorder_z = [], [], []
+            for i in range(0, len(recorder)):
+                recorder_x.append(recorder[i][0])
+                recorder_y.append(recorder[i][1])
+                recorder_z.append(f(np.array(recorder[i]).T))
+            # print(recorder_x, recorder_y, recorder_z)
+            ax.plot3D(recorder_x, recorder_y, recorder_z, color='r', marker= '.')
+            plt.pause(0.1)
     print('result is: ',x)
     print('calculated min value is: ', f(x))
     print('iteration number is: ', iteration_num)
+
+    plt.cla()
+    # visualize surface
+    X = np.arange(-1.5, 1.5, 0.1)
+    Y = np.arange(-1.5, 1.5, 0.1)
+    X, Y = np.meshgrid(X,Y)
+    Z = np.zeros_like(X)
+    for i in range(0, X.shape[0]):
+        for j in range(0, X.shape[1]):
+            Z[i, j] = f(np.array([X[i, j], Y[i, j]]).T)
+    ax.plot_surface(X, Y, Z)
+    # visualize gradient descent
+    recorder_x, recorder_y, recorder_z = [], [], []
+    for i in range(0, len(recorder)):
+        recorder_x.append(recorder[i][0])
+        recorder_y.append(recorder[i][1])
+        recorder_z.append(f(np.array(recorder[i]).T))
+    ax.plot3D(recorder_x, recorder_y, recorder_z, color='r', marker= '.')
+    ax.scatter([recorder_x[-1]], [recorder_y[-1]], [recorder_y[-1]], marker='*')
+    plt.show()
 
 
 if __name__ == "__main__":
